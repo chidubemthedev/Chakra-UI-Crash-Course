a0c150e13aafdc65ab527a84f47e645f7a8c5cd1
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-07-09 03:21:03 +0100

    fork from netninja

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,25 @@
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Container as={"section"} maxWidth={"4xl"} py={"20px"}>
+      <Heading my={"30px"} p={"10px"}>Chakra UI components</Heading>
+      <Text ml={"30px"}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus,
+        fuga?
+      </Text>
+      <Text ml={"30px"} color={"blue.400"} fontWeight={"bold"}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus,
+        fuga?
+      </Text>
+
+      <Box my={"30px"} p={"20px"} bg={"orange"}>
+        <Text color={"white"}>This is a box.</Text>
+      </Box>
+
+      <Box>
+        Hello.
+      </Box>
+    </Container>
+  );
 }
